492152e690f2cdd7bab2bd588102a1fa5cc7f8df
Dane <danekania@gmail.com>
2018-03-12 15:27:50 -0400

    updated: 2018.03.12

---
+++ Drive_Mount.sh
@@ -0,0 +1,9 @@
+
+sudo mount -t cifs -o credentials=/home/yhr8/.smbcredentials_cdc,uid=1000,gid=1000,sec=ntlmssp //data.biotech.cdc.gov/home /media/mui4/
+sudo mount -t cifs -o credentials=/home/yhr8/.smbcredentials_cdc,uid=1000,gid=1000,sec=ntlmssp //data.biotech.cdc.gov/groups/OID/NCIRD/DBD/MVPDB/PDL /media/PDL_GWA/
+sudo mount -t cifs -o credentials=/home/yhr8/.smbcredentials_cdc,uid=1000,gid=1000,sec=ntlmssp //data.biotech.cdc.gov/instruments/18-1-404_PacBio-RSII-RS42252/ /media/DBD_PacBio/
+sudo mount -t cifs -o credentials=/home/yhr8/.smbcredentials_cdc,uid=1000,gid=1000,sec=ntlmssp //cdc.gov/private/L133/mui4 /media/cdc_lg_workspace/
+sudo mount -t cifs -o credentials=/home/yhr8/.smbcredentials_cdc,uid=1000,gid=1000,sec=ntlmssp //cdc.gov/private/M133/mui4 /media/cdc_documents/
+sudo mount -t cifs -o credentials=/home/yhr8/.smbcredentials_cdc,uid=1000,gid=1000,sec=ntlmssp //fspv-107.cdc.gov/CCID_NCIRD_DBD_MVPDB /media/CCID_NCIRD_DBD_MVPDB/
+sudo mount -t cifs -o credentials=/home/yhr8/.smbcredentials_cdc,uid=1000,gid=1000,sec=ntlmssp //fspv-107.cdc.gov/CCID_NCIRD_DBD_MVPDB/PDL/Pertussis/Genomics Data /media/Genomics_Data/
+sudo mount -t cifs -o credentials=/home/yhr8/.smbcredentials_cdc,uid=1000,gid=1000,sec=ntlmssp //data.biotech.cdc.gov/instruments/18-1-218_Illumina-MiSeq-M03242/ /media/MiSeq-M03242/
